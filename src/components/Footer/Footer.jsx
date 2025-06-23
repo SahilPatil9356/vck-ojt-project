b9b9e0da5876d88692c4a268e6289f1816f2905a
Sahil Patil <patilsahil2511@gmail.com>
2025-06-23 18:23:51 +0530

    Sahil first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <div id='footer'>
+        <p id='footer-text'> © 2025 Vivekanand College. All rights reserved.</p>
+
+        </div>
+    )
+}
+export default Footer;

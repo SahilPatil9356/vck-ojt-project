580ad93a2b515ebd73a90ca2194a22b2e2d16ec3
Sahil Patil <patilsahil2511@gmail.com>
2025-06-27 15:35:39 +0530

    nine commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,27 @@ import ContactPage from "./pages/ContactPage";
 
 import ChatbotComponent from './components/Chatbot/ChatbotComponent';
 import AdmissionsPage from './pages/AdmissionPage';
-
-
-
-
+import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup';
+import { useState } from 'react';
 
 const App = () => {
-  return(
+
+const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+  return (
+    <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Sahil Patil"
+          studentPhotoUrl="/images/saniya.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
     <div>
       <Router>
         <Routes>
@@ -33,6 +47,7 @@ const App = () => {
         </Router>
    
     </div>
+    </>
   )
 }
 export default App;
